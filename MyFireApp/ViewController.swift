cf1951cad4c1eb0ef0658768ea53fe57ffa828eb
AlimKuprianov <alimkosheev@gmail.com>
2020-11-10 21:35:14 +0300

    first build

---
+++ MyFireApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFireApp
+//
+//  Created by Алим Куприянов on 10.11.2020.
+//  Copyright © 2020 Алим Куприянов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MyFireApp/Pods-MyFireApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyFireApp : NSObject
+@end
+@implementation PodsDummy_Pods_MyFireApp
+@end
