f78f22859684ecdb4382f2f634db08e1ff333b5a
AlimKuprianov <alimkosheev@gmail.com>
2020-11-12 23:28:32 +0300

    work with Storyboard

---
+++ MyFireApp/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  MyFireApp
+//
+//  Created by Алим Куприянов on 12.11.2020.
+//  Copyright © 2020 Алим Куприянов. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
